df4477baf523624a1985b678ff5ace43afda7b86
zfanzhenhuo <175431850@qq.com>
2016-06-28 18:29:32 +0800

    First commit.

---
+++ app/src/main/java/com/coolweather/app/CoolWeather.java
@@ -0,0 +1,13 @@
+package com.coolweather.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CoolWeather extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
